146e565c525e523bdaac440d560f2610cc9b0d83
Tom <dstart579@gmail.com>
2024-04-23 15:26:16 +0700

    vẽ hình

--- GitOnline/Program.cs
+++ GitOnline/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Chào mấy thằng gà");
 
-Console.WriteLine("con cho Y");
+Console.WriteLine(" ****");
+Console.WriteLine("*    *");
+Console.WriteLine("*    *");
+Console.WriteLine(" ****");
