2357a19bc99c1819251e6391569936d08c8432e2
Minh Y <nguyenhominhy@gmail.com>
2024-04-23 15:26:34 +0700

    minhy_vehinh

--- GitOnline/Program.cs
+++ GitOnline/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Chào mấy thằng gà");
 
+Console.WriteLine("0000");
+Console.WriteLine("0000");
+Console.WriteLine("0000");
+
